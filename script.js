81ea0d57f0924ab9ee7184b10a39601af90659b4
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-19 20:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+  const charFrequency = {};
+  for (const char of str) {
+    if (charFrequency[char]) {
+      charFrequency[char]++;
+    } else {
+      charFrequency[char] = 1;
+    }
+  }
+  for (const char of str) {
+    if (charFrequency[char] === 1) {
+      return char; 
+    }
+  }
+
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
